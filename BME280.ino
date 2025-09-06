49b718a3e49c857cfb749ce567a8085831355f6c
ABE <aliberkanteker06@gmail.com>
2025-09-06 18:49:25 +0300

    Update BME280.ino

--- BME280.ino
+++ BME280.ino
@@ -2,11 +2,45 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
+// BME280 sensörü için nesne oluşturuyoruz
+Adafruit_BME280 bme;
+
+void setup() {
+  Serial.begin(9600);
+  
+  // I2C başlatma
+  Wire.begin();
+
+  // BME280 sensörünü 0x77 adresiyle başlatıyoruz
+  if (!bme.begin(0x77)) {
+    Serial.println("BME280 sensörü bulunamadı!");
+    while (1);
+  }
+
+  Serial.println("BME280 Sensörü Başlatıldı!");
+}
+
 void loop() {
 
   // Sensörden sıcaklık, basınç ve nem verilerini okuması için kodu yazıyoruz.
   float temperature = bme.readTemperature();
   float pressure = bme.readPressure() / 100.0F; // Basınç hPa olarak
   float humidity = bme.readHumidity();
+  
+ // Seri monitörde verileri göster 
+  Serial.print("Sıcaklık: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
 
+  Serial.print("Basınç: ");
+  Serial.print(pressure);
+  Serial.println(" hPa");
+
+  Serial.print("Nem: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.println("-----------------------------------------------");
+  // 2 saniye bekle
+  delay(2000);
 }
