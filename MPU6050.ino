3c8b4deed142af8371ec18219f5b896139d6e8da
ABE <aliberkanteker06@gmail.com>
2025-09-07 22:19:05 +0300

    Update MPU6050.ino

--- MPU6050.ino
+++ MPU6050.ino
@@ -1,15 +1,16 @@
 #include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-int verisayaci=1;
 
+// Creating an object for the MPU6050 sensor ..
 Adafruit_MPU6050 mpu;
 
-void loop() {
+void loop() { 
+// Reading definitions to get axis data from the sensor ..
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-
+  // We're printing data to the serial monitor...
   Serial.print(a.acceleration.x); 
   Serial.print(a.acceleration.y); 
   Serial.println(a.acceleration.z);
@@ -18,5 +19,5 @@ void loop() {
   Serial.print(g.gyro.y); 
   Serial.println(g.gyro.z);
 
-  delay(1000); // veri ölçme aralığı
+  delay(1000); // data measuring range ...
 }
